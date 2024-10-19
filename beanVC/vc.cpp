4257353ba4761a5ff136bfee77007c77db096903
Raghav Bahukhandi <raghapb34n@gmail.com>
2024-10-20 01:57:51 +0530

    added functionality to generate ordered file content with multiple commits containing delta changes

--- beanVC/vc.cpp
+++ beanVC/vc.cpp
@@ -3,9 +3,37 @@
 #include <filesystem>
 #include <string>
 #include <vector>
+#include <ctime>
 
 using namespace std::filesystem;
 using namespace std;
+
+int convertCharToInt(const char* c)
+{
+    if ((*c == '-') && (*(c+1) == '1')) {
+        return -1;
+    }
+
+    const char nums[] = "0123456789";
+    int val = 0;
+
+    while(*c != '\0') 
+    {
+        int i = 0;
+
+        for(i; nums[i] != '\0'; i++) {
+            if (nums[i] == *c) {
+                break;
+            }
+        }
+
+        val = val*10;
+        val += i;
+        c++;
+    }
+
+    return val;
+}
 
 class FileNode
 {
@@ -18,7 +46,7 @@ class FileNode
 
     // when a new filenode is initialized, it should go through all the files in the objects directory and look for 
 
-    void trim(string &s, string &result)
+    static void trim(string &s, string &result)
     {
         size_t index_p = s.find_first_of('+');
         size_t index_m = s.find_first_of('-');
@@ -38,6 +66,46 @@ class FileNode
         return;
     }
 
+    int checkPlaceToBeInserted(vector<string> prevCommitContent, vector<string> currentFileContent, int index, int indexToBeInsertedAt)
+    {
+        for(int x = 0; x < prevCommitContent.size(); x++) 
+        {
+            if (prevCommitContent[x] == "--START--")
+            {
+                if (prevCommitContent[x+1] == filename)
+                {
+                    // by this point we have found the commit for this version. The line above which the string is to be inserted is at 
+                    // prevCommitContent[x+1+index]. now we check sentences in currentFileContent from indexToBeInsertedAt. 
+                    // since no changes have been made to the content below line at index (we are moving iteratively), lines should match
+                    // this comparision goes on till prevCommitContent[x+1+index+i] == "--END--"
+                    int i = index;
+                    int j = indexToBeInsertedAt;
+                    int looped = 0;
+
+                    while(prevCommitContent[x+1+i] != "--END--")
+                    {
+                        looped = 1;
+                        string trimmedLine;
+                        FileNode::trim(prevCommitContent[x+1+i], trimmedLine);
+                        if(currentFileContent[j] != trimmedLine) {
+                            return 0;
+                        }
+                        j++;
+                        i++;
+                    }
+
+                    if (!looped) {
+                        cerr << "Ordering index out of bounds for previous commit file.\n";
+                        exit(1);
+                    }
+
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+
     void orderData()
     {
         // code to get file data by combining data of all the commits. it works as follows:
@@ -45,21 +113,38 @@ class FileNode
         // this means that all the stuff currently goes before the EOF meaning all the stuff goes below whatever was there before.
 
         // if the order decider has a value of K, this means that this particular line goes before the Kth line from previous commit
-        string pathToObj = current_path().string() + "/.beanVC/objects";
+        ifstream lastCommitFile = ifstream(".beanVC/lastCommitVersion.txt", ios_base::in);
+        char c[2];
+        lastCommitFile.read(c, 2);
+        lastCommitFile.close();
 
-        directory_iterator objIt = directory_iterator(pathToObj);
-        directory_iterator e = end(objIt);
+        int lastCommitVersion = convertCharToInt(c);
+        int currentFileVersion = 0;
         vector<string> fileContent;
+        vector<string> prevCommitContent;
 
-        while(objIt != e)
+        while(currentFileVersion <= lastCommitVersion)
         {
-            string currentFilePath = objIt->path().string();
+            
+            string currentFilePath = ".beanVC/objects/";
+            vector<string> currentCommitContent;
+
+            if (currentFileVersion < 10) 
+            {
+                currentFilePath.push_back('0');
+                currentFilePath += to_string(currentFileVersion);
+            } else {
+                currentFilePath += to_string(currentFileVersion);
+            }
+            currentFilePath += ".bin";
+
             string line;
             ifstream currentFile = ifstream(currentFilePath, ios_base::in);
             int insideAnotherFile = 0;
 
             while(getline(currentFile, line))
             {
+                currentCommitContent.push_back(line);
                 if (insideAnotherFile)
                 {
                     if (line == "--END--")
@@ -70,6 +155,7 @@ class FileNode
                 } else if (line == "--START--")
                 {
                     getline(currentFile, line);
+                    currentCommitContent.push_back(line);
                     if (line == filename) 
                     {
                         insideAnotherFile = 0;
@@ -88,11 +174,50 @@ class FileNode
                         string result;
                         trim(line, result);
                         fileContent.push_back(result);
+                    } else if (line[1] >'0' && line[1] < '9') {
+                        char a[2] = {line[1], '\0'};
+                        int index = convertCharToInt(a);
+                        string lowerBound; 
+                        for(int x = 0; x < prevCommitContent.size(); x++) 
+                        {
+                            if (prevCommitContent[x] == "--START--")
+                            {
+                                if (prevCommitContent[x+1] == filename)
+                                {
+                                    lowerBound = string(prevCommitContent[x+1+index]);
+                                    break;
+                                }
+                            }
+                        }
+
+                        int indexToBeInsertedAt = -1;
+                        string trimmedLowerBound;
+                        FileNode::trim(lowerBound, trimmedLowerBound);
+
+                        for(int j = fileContent.size()-1; j >= 0; j--)
+                        {
+                            int shouldBeInsertedHere = checkPlaceToBeInserted(prevCommitContent, fileContent, index, j);
+                            if (shouldBeInsertedHere) {
+                                indexToBeInsertedAt = j;
+                                break;
+                            }
+                        }
+
+                        if (indexToBeInsertedAt == -1)
+                        {
+                            cerr << "The line that commit file " << currentFileVersion << " was using for ordering content does not exist in previous commit file.\n";
+                            exit(1);
+                        }
+
+                        string result;
+                        trim(line, result);
+                        fileContent.insert(fileContent.begin()+indexToBeInsertedAt, result);
                     }
                 }
             }
             currentFile.close();
-            objIt++;
+            currentFileVersion++;
+            prevCommitContent = vector(currentCommitContent);
         }
 
         for(int i = 0; i < fileContent.size(); i++)
@@ -173,37 +298,12 @@ void stageChanges()
             }
         }
         stagingFile.close();
+    } else {
+
     }
 }
 
-int convertCharToInt(const char* c)
-{
-    if ((*c == '-') && (*(c+1) == '1')) {
-        return -1;
-    }
-
-    const char nums[] = "0123456789";
-    int val = 0;
-
-    while(*c != '\0') 
-    {
-        int i = 0;
-
-        for(i; nums[i] != '\0'; i++) {
-            if (nums[i] == *c) {
-                break;
-            }
-        }
-
-        val = val*10;
-        val += i;
-        c++;
-    }
-
-    return val;
-}
-
-void addStagedChanges()
+void addStagedChanges(string commitMessage)
 {
     // this function checks out the objects directory looking for a tempStaging.bin file, if found, it writes that data in tempStaging.bin
     // else it throws error and exits 
@@ -228,12 +328,12 @@ void addStagedChanges()
     if (commitVersion < 10)
     {
         string s = ".beanVC/objects/0";
-        s += commitVersion;
+        s += to_string(commitVersion);
         s += ".bin";
         stagingFile = ofstream(s, ios_base::out);
     } else {
         string s = ".beanVC/objects/";
-        s += commitVersion;
+        s += to_string(commitVersion);
         s += ".bin";
         stagingFile = ofstream(s, ios_base::out);
     }
@@ -249,7 +349,35 @@ void addStagedChanges()
     stagingFile.close();
     tempStagingFile.close();
     path tempStagingFilePath = path(".beanVC/objects/tempStaging.bin");
+    string logPathString = ".beanVC/logs/";
     remove(tempStagingFilePath);
+
+    ofstream commitVersionFile = ofstream(".beanVC/lastCommitVersion.txt", ios_base::out);
+
+    if (commitVersion < 10)
+    {
+        logPathString.push_back('0');
+        commitVersionFile.write("0", 1);
+        commitVersionFile.write(to_string(commitVersion).c_str(), 1);
+    } else {
+        commitVersionFile.write(to_string(commitVersion).c_str(), 2);
+    }
+    logPathString += to_string(commitVersion);
+    logPathString += "_log.txt";
+    commitVersionFile.close();
+
+
+    time_t timestamp = time(0);
+    string t = to_string(timestamp);
+
+    ofstream logFile = ofstream(logPathString, ios_base::out);
+    logFile.write(t.c_str(), t.size());
+    logFile.write("\n", 1);
+    logFile.write(commitMessage.c_str(), commitMessage.size());
+
+    logFile.close();
+
+    return;
 }
 
 void initRepo()
@@ -301,10 +429,14 @@ int main(int argc, char *argv[])
         stageChanges();
     } else if (argument == "commit")
     {
-        addStagedChanges();
+        string commitMessage = string(argv[2]);
+        addStagedChanges(commitMessage);
     } else if (argument == "show")
     {
         showFiles();
+    } else if (argument == "test")
+    {
+        cout << string(argv[2]);
     }
     return 0;
 }
